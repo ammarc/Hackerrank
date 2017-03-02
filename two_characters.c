bce0171fd6ee97cd42cb46659d996f8b3c5f1130
Ammar Ahmed <ammar@Ammars-MacBook-Pro.local>
2017-03-03 05:10:42 +1100

    :postbox: signature added

--- two_characters.c
+++ two_characters.c
@@ -1,15 +1,11 @@
+/* Code by Ammar Ahmed */
+
 #include <math.h> 
-
 #include <stdio.h> 
-
 #include <string.h> 
-
 #include <stdlib.h> 
-
 #include <assert.h> 
-
 #include <limits.h> 
-
 #include <stdbool.h> 
 
   
